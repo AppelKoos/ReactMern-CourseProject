a3d3491b155b8c0428556c58e0028b8f8f4c27f1
Johan <joahnvdm2000@gmail.com>
2020-11-20 14:06:39 +0200

    Completed Section 5, React front end
    Finished up until Section 5 lecture 76 (end)

--- react-mern-courseproject/src/App.js
+++ react-mern-courseproject/src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import React, { useState, useCallback } from 'react';
+import {
+	BrowserRouter as Router,
+	Redirect,
+	Route,
+	Switch,
+} from 'react-router-dom';
 
-import MainNavigation from './shared/components/Navigation/MainNavigation'
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Users from './user/pages/Users';
-import NewPlaces from './places/pages/NewPlace'
-import UserPlaces from './places/pages/UserPlaces'
-import UpdatePlace from './places/pages/UpdatePlace'
+import NewPlaces from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
 
 function App() {
-  return <Router>
-    <MainNavigation />
-    <main>
-      <Switch>
-        <Route path="/" exact > <Users /> </Route>
-        <Route path="/:userId/places"> <UserPlaces /> </Route>
-        <Route path="/places/new" exact > <NewPlaces /> </Route>
-        <Route path="/places/:placeId" exact > <UpdatePlace /> </Route>
-        <Redirect to="/" />
-      </Switch>
-    </main>
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  </Router>
+	const login = useCallback(() => {
+		setIsLoggedIn(true);
+	}, []);
+	const logout = useCallback(() => {
+		setIsLoggedIn(false);
+	}, []);
+
+	let routes;
+	if (isLoggedIn) {
+		routes = (
+			<Switch>
+        <Route path="/" exact>
+					<Users />
+				</Route>
+        <Route path="/:userId/places">
+					<UserPlaces />
+				</Route>
+				<Route path="/places/new" exact>
+					<NewPlaces />
+				</Route>
+				<Route path="/places/:placeId" exact>
+					<UpdatePlace />
+				</Route>
+				<Redirect to="/" />
+			</Switch>
+		);
+	} else {
+		routes = (
+			<Switch>
+				<Route path="/" exact>
+					<Users />
+				</Route>
+				<Route path="/:userId/places">
+					<UserPlaces />
+				</Route>
+				<Route path="/auth">
+					<Auth />
+				</Route>
+				<Redirect to="/auth" />
+			</Switch>
+		);
+	}
+
+	return (
+		<AuthContext.Provider
+			value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+		>
+			<Router>
+				<MainNavigation />
+				<main>{routes}</main>
+			</Router>
+		</AuthContext.Provider>
+	);
 }
 
 export default App;
--- react-mern-courseproject/src/shared/components/Navigation/NavLinks.js
+++ react-mern-courseproject/src/shared/components/Navigation/NavLinks.js
@@ -1,22 +1,31 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 
-import './NavLinks.css'
+import { AuthContext } from '../../context/auth-context';
+import './NavLinks.css';
 
-const NavLinks = props => {
-    return <ul className="nav-links">
-        <li>
-            <NavLink to="/" exact >ALL USERS</NavLink>
-        </li>
-        <li>
-            <NavLink to="/u1/places">MY PLACES</NavLink>
-        </li>
-        <li>
-            <NavLink to="/places/new">ADD PLACE</NavLink>
-        </li>
-        <li>
-            <NavLink to="/auth">AUTHENTICATED</NavLink>
-        </li>
-    </ul>
-}
-export default NavLinks
+const NavLinks = (props) => {
+	const auth = useContext(AuthContext);
+	return (
+		<ul className="nav-links">
+			<li>
+				<NavLink to="/" exact>
+					ALL USERS
+				</NavLink>
+			</li>
+			<li>{auth.isLoggedIn && <NavLink to="/u1/places">MY PLACES</NavLink>}</li>
+			<li>
+				{auth.isLoggedIn && <NavLink to="/places/new">ADD PLACE</NavLink>}
+			</li>
+			<li>{!auth.isLoggedIn && <NavLink to="/auth">AUTHENTICATED</NavLink>}</li>
+			{auth.isLoggedIn && (
+				<li>
+					<button onClick={auth.logout}>
+						LOGOUT
+					</button>
+				</li>
+			)}
+		</ul>
+	);
+};
+export default NavLinks;
